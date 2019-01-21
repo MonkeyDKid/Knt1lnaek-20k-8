6f2f9af1d79a858937c237b50e121d5e373c9371
DESKTOP-E4J4KK1\MonkeyDKid <anjar.d.saputra93@gmail.com>
2019-01-21 14:12:14 +0900

    Finishing Crafting Scene

--- Assets/CraftingManager.cs
+++ Assets/CraftingManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
 public class CraftingManager : MonoBehaviour {
-
+float myFloat;
 public GameObject[] SeqObject;
 	// Use this for initialization
 	void Start () {
@@ -27,6 +27,7 @@ public void ABC()
 {
 		
 		SeqObject[4].transform.DOScale(new Vector3(1.5f,1.5f,1),1).OnComplete(()=>{
+			SeqObject[10].GetComponent<Image>().DOFillAmount(1,7).SetEase(Ease.InCubic);
 			SeqObject[8].transform.DOScale(new Vector3(.4f,.4f,1),1);
 			SeqObject[5].SetActive(true);
 			SeqObject[6].GetComponent<SpriteRenderer>().DOFade(0,2);
@@ -35,7 +36,7 @@ public void ABC()
 public void ShowItem()
 {
 	SeqObject[9].transform.DOScale(new Vector3(4f,4f,4f),0);
-	SeqObject[9].transform.DORotate(new Vector3(0,360,0),1f).SetLoops(-1, LoopType.Incremental);
+	SeqObject[11].SetActive(true);
 	SeqObject[8].GetComponent<SpriteRenderer>().DOFade(0,0);
 	SeqObject[7].GetComponent<SpriteRenderer>().DOFade(1,0f);
 }
