44c09a4c54857361e3690ab0e4530b6b6c07ca59
DESKTOP-E4J4KK1\MonkeyDKid <anjar.d.saputra93@gmail.com>
2019-02-07 17:14:08 +0900

    add osm query in berburuhantu scene

--- Assets/Infinity Code/Online maps/Examples (API usage)/OSMRequestExample.cs
+++ Assets/Infinity Code/Online maps/Examples (API usage)/OSMRequestExample.cs
@@ -8,12 +8,14 @@ using UnityEngine;
 
 namespace InfinityCode.OnlineMapsExamples
 {
+    
     /// <summary>
     /// Example of how to make a request to Open Street Map Overpass API and handle the response.
     /// </summary>
     [AddComponentMenu("Infinity Code/Online Maps/Examples (API Usage)/OSMRequestExample")]
     public class OSMRequestExample : MonoBehaviour
     {
+        public string requestData3;
         private void Start()
         {
             // Get map corners
@@ -24,7 +26,7 @@ namespace InfinityCode.OnlineMapsExamples
             string requestData = String.Format("node({0},{1},{2},{3});way(bn)[{4}];(._;>;);out;",
                 bottomRight.y, topLeft.x, topLeft.y, bottomRight.x, "'highway'~'primary|residential'");
             string requestData2 =  String.Format("node[shop=convenience]({0},{1},{2},{3});(._;>;);out;",bottomRight.y, topLeft.x, topLeft.y, bottomRight.x);
-            string requestData3 = String.Format("area(3600304751)->.searchArea; node[shop=convenience](area.searchArea)({0},{1},{2},{3});(._;>;); out;",bottomRight.y, topLeft.x, topLeft.y, bottomRight.x);
+            requestData3 = String.Format("area(3600304751)->.searchArea; node[shop=convenience](area.searchArea)({0},{1},{2},{3});(._;>;); out;",bottomRight.y, topLeft.x, topLeft.y, bottomRight.x);
             // Send request and subscribe to complete event
             OnlineMapsOSMAPIQuery.Find(requestData3).OnComplete += OnComplete;
         }
